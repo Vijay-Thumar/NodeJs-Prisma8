6c6c8bc1b7f8c3613f735d859e116c88fbf070f3
vijay-thumar <vijaythumar1020@gmail.com>
2026-09-19 20:43:20 +0530

    ref: added prettier and added bcryptjs for the encryption

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,35 @@
 import { prisma } from "../config/db.js"
+import bcrypt from "bcryptjs"
 
 const register = async (req, res) => {
     const { name, email, password } = req.body
 
     // Check if user is already registered
-    const userExists = await prisma.orm.public.User.findUnique({
-        where: { email: email }
-    })
+    const userExists = await prisma.orm.public.User.where({
+        email: email
+    }).first()
 
     if (userExists) {
         return res.status(400).json({ error: "User already exists with the email id" })
     }
 
+    // Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = await prisma.orm.public.User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
 
 
-    res.json(body)
+    res.status(201).json({
+        status: 'sucess', data: {
+            user: { id: user.id, name, email }
+        }
+    })
 }
 
 
